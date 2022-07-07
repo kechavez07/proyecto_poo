5274970d226480646777887bd44bdca31cbf6e09
kleber <kleberenriqueos@gmail.com>
2022-07-07 10:35:45 -0500

    jajajja

--- Restaurante/RestauranteApp/src/modelo/Conexion.java
+++ Restaurante/RestauranteApp/src/modelo/Conexion.java
@@ -1,14 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package modelo;
 
-/**
- *
- * @author Jeimy
- */
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
+
 public class Conexion {
+        DB baseDatos;
+    DBCollection coleccion;
     
+    //Constructor
+    public Conexion(){
+        MongoClient mongo=new MongoClient("localhost",27017);
+        baseDatos= mongo.getDB("clinica");
+        coleccion= baseDatos.getCollection("proveedores");
+        System.out.println("Conexion aBD ok");
+        
+    }
 }
+
