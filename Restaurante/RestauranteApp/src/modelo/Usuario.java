af4decefc35957ad5da2079306a4fd1a094361a6
Camilo Andrés Orrico Emme <99036443+caorrico@users.noreply.github.com>
2022-07-10 15:10:12 -0500

    actualizo

--- Restaurante/RestauranteApp/src/modelo/Usuario.java
+++ Restaurante/RestauranteApp/src/modelo/Usuario.java
@@ -10,5 +10,51 @@ package modelo;
  * @author Jeimy
  */
 public class Usuario {
-    
+    private String nombre;
+    private String apellido;
+    private String id;
+    private String contraseña;
+    private String email;
+    private String numeroCelular;
+    private Fecha fechaNacimiento;
+
+    public Usuario(String nombre, String apellido, String id, String contraseña, String email, String numeroCelular, Fecha fechaNacimiento) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.id = id;
+        this.contraseña = contraseña;
+        this.email = email;
+        this.numeroCelular = numeroCelular;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getNumeroCelular() {
+        return numeroCelular;
+    }
+
+    public Fecha getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+   
 }
--- Restaurante/RestauranteApp/src/modelo/UsuarioDAO.java
+++ Restaurante/RestauranteApp/src/modelo/UsuarioDAO.java
@@ -5,10 +5,23 @@
  */
 package modelo;
 
+import com.mongodb.BasicDBObject;
+
 /**
  *
  * @author Jeimy
  */
 public class UsuarioDAO {
-    
+    public void insertarUsuario(Usuario p){
+        Conexion objCon=new Conexion();
+        BasicDBObject documento= new BasicDBObject();
+        documento.put("nombre", p.getNombre());
+        documento.put("apellido", p.getApellido());
+        documento.put("id", p.getId());
+        documento.put("contraseña", p.getContraseña());
+        documento.put("email", p.getEmail());
+        documento.put("numeroCelular", p.getNumeroCelular());
+        documento.put("fecha", p.getFechaNacimiento());
+        objCon.coleccion.insert(documento);
+    }
 }
